4eda3b905b22e5da585e62bd1d0f58797d565ab4
HRMS BACKEND LARAVEL <rajarajeswari27072001@gmail.com>
2026-05-16 17:46:45 +0530

    Initial commit

--- app/config.py
+++ app/config.py
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
     # Google OAuth
     GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID", "")
     GOOGLE_CLIENT_SECRET: str = os.getenv("GOOGLE_CLIENT_SECRET", "")
-    GOOGLE_REDIRECT_URI: str = os.getenv("GOOGLE_REDIRECT_URI", "http://localhost:8000/auth/google/callback")
+    GOOGLE_REDIRECT_URI: str = os.getenv("GOOGLE_REDIRECT_URI", "https://zadtowing.rootreachtechnologies.com/auth/google/callback")
     
     # Email
     SMTP_SERVER: str = os.getenv("SMTP_SERVER", "smtp.gmail.com")
